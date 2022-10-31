44c1ea84bc2e613d6a17823a9e0b7c36392a8af2
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-11-01 00:03:56 +0300

    initial commit

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,16 @@
+require_relative "article"
 class Magazine
   attr_accessor :name, :category
 
-
+@@all magazine =[]
   def initialize(name, category)
     @name = name
     @category = category
+    @@all  << self
 
   end
 
+def self.all
+end
 
 end
